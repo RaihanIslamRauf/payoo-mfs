b8356d1364af70a4a7004feae4a0c9ed54ec1314
RaihanIslamRauf <md.raufislam27@gmail.com>
2024-09-22 01:26:22 +0600

    added add money and cash out functionality

--- js/home.js
+++ js/home.js
@@ -2,6 +2,8 @@
 /**
  * S-1: add event handler prevent page reload after form submit
  * S-2 : get money to be added to the account balance
+ * get the pin number
+ * S-3 : verify the pin number
  */
 // step 1: add an event handler to the add money button inside the form
 document.getElementById('btn-add-money').addEventListener('click',function(event){
@@ -15,4 +17,27 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+
+    // step 3: verify pin number
+    // wrong way to validate the pin number
+    if(pinNumberInput==='1234'){
+        console.log('adding money to your account');
+
+        // step 4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step 5: add addMoneyInput with Balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step 6: update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+        
+    }
+    else{
+        alert('Failed to add money! Please try again.');
+    }
 });
